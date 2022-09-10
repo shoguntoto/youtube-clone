4625d45062a0f99d939eb7cf39ea4bb5e1a59187
shoguntoto <85441789+shoguntoto@users.noreply.github.com>
2022-09-10 12:03:52 +0000

    Initial commit

---
+++ src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': "14d4baf57dmsh72dce338a34c24fp191a22jsn1d801a2353be",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
