4a6440f27573dd5f4c85863b216c1069946112f9
shoguntoto <85441789+shoguntoto@users.noreply.github.com>
2022-09-10 13:44:36 +0000

    second-commit

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import { Box, CardContent, CardMedia, Typography } from '@mui/material';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import { Link } from 'react-router-dom';
+import { demoProfilePicture } from '../utils/constants';
 
-const ChannelCard = () => {
+const ChannelCard = ({ channelDetail }) => {
   return (
-    <div>ChannelCard</div>
+  <Box
+    sx={{
+      boxShadow: 'none',
+      borderRadius: '20px',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      width: { xs: '356px', md: '320px' },
+      height: '326px',
+      margin: 'auto'
+    }}
+  >
+   <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+     <CardContent  sx = {{display: 'flex', flexDirection: 'column', justifyContent: 'center', textAlign: 'center', color: '#fff'}}>
+          <CardMedia
+            image={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt = {channelDetail?.snippet?.title}
+            sx = {{ borderRadius: '50%', height: '180px', width: '180px', mb: 2, border: '1px solid #e3e3e3'}}
+        /> 
+
+          <Typography variant = "h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircleIcon sx = {{fontSize: 14, color: 'gray', ml: '5px'}} />
+          </Typography>
+       {channelDetail?.statistics?.subscriberCount && (
+        <Typography>
+          {parseInt(channelDetail?.statistics?.subscriberCount).toLocaleString()} Abonnés
+        </Typography>
+       )}
+        </CardContent>
+      </Link>
+    </Box>
   )
 }
 
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,9 +3,11 @@ import { Stack, Box } from "@mui/material";
 
 import { ChannelCard, VideoCard } from "./";
 
-const Videos = ({ videos }) => {
-    return (
-    <Stack direction = "row" flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
+const Videos = ({ videos, direction }) => {
+  if(!videos?.length) return;
+  
+  return (
+    <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
       {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} /> }
